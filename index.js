66c9494abe536b27d7df041266af403e9a50b7bf
guanlun <guanlunzhao@gmail.com>
2015-01-06 22:17:09 +0800

    added mongodb

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express');
+var mongo = require('mongoskin');
+var db = mongo.db('mongodb://localhost:27017/words', {native_parser: true});
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/words', function(req, res) {
+    var db = req.db;
+    db.collection('wordlist').find().toArray(function(err, items) {
+        res.json(items);
+    });
 });
 
 app.listen(app.get('port'), function() {
