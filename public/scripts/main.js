4b03c8f414b736c908bcbb44d7937f0c9ec74179
guanlun <guanlunzhao@gmail.com>
2015-01-11 12:59:41 +0800

    added bootstrap

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,7 +13,7 @@ wordListApp.config(['$routeProvider', function($routeProvider) {
     });
 }]);
 
-wordListApp.service('wordListSrvc', function($http) {
+wordListApp.service('wordListSrvc', function($http, $q) {
     function getWordLists() {
         var request = $http({
             method: 'get',
@@ -56,7 +56,7 @@ wordListApp.service('wordListSrvc', function($http) {
     }
 
     function err(res) {
-        return $q.reject(res.data.message);
+        return $q.reject(res.data);
     }
 
     return ({
@@ -69,7 +69,7 @@ wordListApp.service('wordListSrvc', function($http) {
 
 wordListApp.controller('mainCtrl', function($scope, $routeParams, wordListSrvc) {
     $scope.wordLists = [];
-    $scope.currListId = 0;
+    $scope.currListName = '';
 
     $scope.editing = false;
 
@@ -78,14 +78,23 @@ wordListApp.controller('mainCtrl', function($scope, $routeParams, wordListSrvc) 
     });
 
     $scope.$on('$routeChangeSuccess', function(ev, current, pre) {
-        $scope.currListId = parseInt(current.params.id);
+        $scope.currListName = current.params.id;
     });
 
     $scope.createWordList = function(listName) {
-        wordListSrvc.createWordList(listName)
-        .then(function(data) {
-            console.log(data);
+        $scope.wordLists.push({
+            list_name: listName,
+            words: []
         });
+
+        wordListSrvc.createWordList(listName).then(
+            function(data) {
+                console.log(data);
+            }, function(err) {
+                alert(err.message);
+                $scope.wordLists.pop();
+            }
+        );
     };
 
     $scope.insertWord = function(title, meaning) {
@@ -97,7 +106,7 @@ wordListApp.controller('mainCtrl', function($scope, $routeParams, wordListSrvc) 
         $scope.getCurrWordList().push(word);
 
         wordListSrvc.insertWord({
-            listId: $scope.currListId,
+            list_name: $scope.currListName,
             word: word
         }).then(function(data) {
             console.log(data);
@@ -111,17 +120,16 @@ wordListApp.controller('mainCtrl', function($scope, $routeParams, wordListSrvc) 
     $scope.updateWord = function(index) {
         $scope.editing = false;
         console.log(index);
-        // console.log($scope.wordLists[$scope.currListId].words[index]);
     };
 
-    $scope.deleteWord = function(id) {
+    $scope.deleteWord = function(title) {
         var currList = $scope.getCurrWordList();
-        var idx = currList.indexOf(_.findWhere(currList, { _id: id }));
+        var idx = currList.indexOf(_.findWhere(currList, { title: title }));
         currList.splice(idx, 1);
 
         wordListSrvc.deleteWord({
-            listId: $scope.currListId,
-            id: id
+            list_name: $scope.currListName,
+            title: title
         }).then(function(data) {
             console.log(data);
         });
@@ -129,7 +137,7 @@ wordListApp.controller('mainCtrl', function($scope, $routeParams, wordListSrvc) 
 
     $scope.getCurrWordList = function() {
         if ($scope.wordLists.length != 0) {
-            return _.findWhere($scope.wordLists, { _id: $scope.currListId }).words;
+            return _.findWhere($scope.wordLists, { list_name: $scope.currListName }).words;
         }
     };
 
